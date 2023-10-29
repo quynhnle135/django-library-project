ceef6224a62adb9dd6da2f3a2b8c16c6be1ff69f
Quinn Le <quynhnle135@gmail.com>
2023-10-29 15:18:26 -0400

    Wrote CRUD operation views and added templates

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-
+    path("books/", views.BookListView.as_view(), name="book-list"),
+    path("books/<int:pk>/", views.BookDetailView.as_view(), name="book-detail"),
+    path("books/add/", views.BookAddView.as_view(), name="book-add"),
+    path("books/<int:pk>/update/", views.BookUpdateView.as_view(), name="book-update"),
+    path("books/<int:pk>/delete/", views.BookDeleteView.as_view(), name="book-delete"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Book
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "books"
+    template_name = "base/book_list.html"
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = "base/book_detail.html"
+
+
+class BookAddView(generic.CreateView):
+    model = Book
+    fields = "__all__"
+    template_name = "base/book_form.html"
+    success_url = reverse_lazy("book-list")
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = "__all__"
+    template_name = "base/book_update_form.html"
+    success_url = reverse_lazy("book-list")
+
+
+class BookDeleteView(generic.DeleteView):
+    model = Book
+    template_name = "base/book_confirm_delete_form.html"
+    success_url = reverse_lazy("book-list")
